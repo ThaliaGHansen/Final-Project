dfbae07e1d949e1196946f21bce043a3e86f1296
Admin <admin@Thalias-MacBook-Air.local>
2019-11-08 18:04:55 -0700

    finalpp

--- app/app.py
+++ app/app.py
@@ -13,12 +13,26 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-
-
 @app.route("/")
 def index():
     """Return the homepage."""
     return render_template("index.html")
+
+@app.route("/obsgraph")
+def obsgraph():
+    """Return the OBS Graph page."""
+    return render_template("obsgraph.html")
+
+@app.route("/stacked")
+def stacked():
+    """Return the Stacked Graph page."""
+    return render_template("stacked.html")
+
+
+@app.route("/fun")
+def fun():
+    """Return the code page."""
+    return render_template("fun.html")
 
 if __name__ == "__main__":
     app.run()
